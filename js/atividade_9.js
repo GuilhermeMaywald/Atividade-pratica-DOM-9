e62690521f7c5c6b47ec89db7f3d6a4fc08ad25a
guilherme maywald <guilhermemaywald@gmail.com>
2022-10-06 20:34:38 -0300

    Correção final

--- js/atividade_9.js
+++ js/atividade_9.js
@@ -1,34 +1,29 @@
 let listaProdutos = [
     {
-        id: 1,
         produto: "Leite Barnabé",
         medida: "litro",
         quantidade: 5,
         valor: 7.8
     },
     {
-        id: 2,
         produto: "Alcatra Friboi",
         medida: "kg",
         quantidade: 1.896,
         valor: 40
     },
     {
-        id: 3,
         produto: "Refrigerante Toca Polar",
         medida: "pet 2 litros",
         quantidade: 3,
         valor: 8.99
     },
     {
-        id: 4,
         produto: "Arroz Tio Chicão",
         medida: "Pacote 1kg",
         quantidade: 4,
         valor: 5.99
     },
     {
-        id: 5,
         produto: "Peito de frango Hot Chicken",
         medida: "kg",
         quantidade: 5,
@@ -45,7 +40,6 @@ function listarProdutosNaTabela() {
     }, 0);
 
     listaProdutos.forEach((produto) => { 
-        // trTds += `<tr><td>${produto.id}</td>`;
         trTds += `<tr><td>${produto.produto}</td>`;
         trTds += `<td>${produto.medida}</td>`;
         trTds += `<td>${produto.quantidade}</td>`;
